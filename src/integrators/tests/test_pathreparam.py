56e70f01e1babf7d504911ad67e0145d1876f4a3
Sebastien Speierer <sebastieneps@gmail.com>
2020-05-22 14:06:42 +0200

    Update pathreparam tests

--- src/integrators/tests/test_pathreparam.py
+++ src/integrators/tests/test_pathreparam.py
@@ -5,6 +5,9 @@ import numpy as np
 
 from mitsuba.python.test.util import fresolver_append_path
 
+mitsuba.set_variant("scalar_rgb")
+from mitsuba.core import ScalarTransform4f
+
 def write_gradient_image(grad, name):
     """Convert signed floats to blue/red gradient exr image"""
     from mitsuba.core import Bitmap
@@ -75,21 +78,22 @@ def compute_groundtruth(make_scene, integrator, spp, passes, epsilon):
     return image, gradient[:, :, [0]]
 
 
-
-diff_integrator_default = """<integrator type="pathreparam">
-                                 <integer name="max_depth" value="2"/>
-                             </integrator>"""
-
-ref_integrator_default = """<integrator type="path">
-                                <integer name="max_depth" value="2"/>
-                            </integrator>"""
+diff_integrator_default = { "type" : "pathreparam", "max_depth" : 2 }
+ref_integrator_default = { "type" : "path", "max_depth" : 2 }
 
 
-def check_finite_difference(test_name, make_scene, get_diff_params,
-                            diff_integrator=diff_integrator_default, diff_spp=4, diff_passes=10,
-                            ref_integrator=ref_integrator_default, ref_spp=128, ref_passes=10, ref_eps=0.001,
+def check_finite_difference(test_name,
+                            make_scene,
+                            get_diff_params,
+                            diff_integrator=diff_integrator_default,
+                            diff_spp=4,
+                            diff_passes=10,
+                            ref_integrator=ref_integrator_default,
+                            ref_spp=128,
+                            ref_passes=10,
+                            ref_eps=0.002,
                             error_threshold=0.1):
-
+    """Compare resulting image and image gradient with finite difference method"""
     from mitsuba.core import Bitmap, Struct
     from mitsuba.python.autodiff import render
 
@@ -120,82 +124,104 @@ def check_finite_difference(test_name, make_scene, get_diff_params,
         Bitmap(img).write('%s_img.exr' % test_name)
         assert False
 
-# -----------------------------------------------------------------------
-# -------------------------------- TESTS --------------------------------
-# -----------------------------------------------------------------------
+
+def sensor(spp):
+    return {
+        "type" : "perspective",
+        "near_clip" : 0.1,
+        "far_clip" : 2800,
+        "focus_distance" : 1000,
+        "fov" : 60,
+        "to_world" : ScalarTransform4f.look_at([0, 0, 4], [0, 0, 0], [0, 1, 0]),
+        "sampler" : {
+            "type" : "independent",
+            "sample_count" : spp
+        },
+        "film" : {
+            "type" : "hdrfilm",
+            "width" :  48,
+            "height" : 48,
+            "filter" : { "type" : "box" }
+        }
+    }
+
+
+plane_mesh = {
+    "type" : "obj",
+    "id" : "planemesh",
+    "to_world" : ScalarTransform4f.scale(2.0),
+    "filename" : "resources/data/obj/xy_plane.obj"
+}
+
+
+def update_vertex_buffer(scene, object_name, diff_trafo):
+    """Apply the given transformation to mesh vertex positions and call update scene"""
+    from mitsuba.core import UInt32, Point3f
+    from mitsuba.python.util import traverse
+
+    params = traverse(scene)
+    vertex_positions_buf = params[object_name + '.vertex_positions_buf']
+
+    idx = UInt32.arange(params[object_name + '.vertex_count'])
+    vertex_positions = Point3f(ek.gather(vertex_positions_buf, 3 * idx + 0),
+                                ek.gather(vertex_positions_buf, 3 * idx + 1),
+                                ek.gather(vertex_positions_buf, 3 * idx + 2))
+
+    vertex_positions_t = diff_trafo.transform_point(vertex_positions)
+
+    ek.scatter(vertex_positions_buf, vertex_positions_t.x, 3 * idx + 0)
+    ek.scatter(vertex_positions_buf, vertex_positions_t.y, 3 * idx + 1)
+    ek.scatter(vertex_positions_buf, vertex_positions_t.z, 3 * idx + 2)
+
+    params[object_name + '.vertex_positions_buf'] = vertex_positions_buf
+
+    # Update the scene
+    params.update()
+
+
+# # -----------------------------------------------------------------------
+# # -------------------------------- TESTS --------------------------------
+# # -----------------------------------------------------------------------
+
 
 @pytest.mark.slow
 def test01_light_position(variant_gpu_autodiff_rgb):
-    from mitsuba.core import Float, Vector3f, Transform4f
-    from mitsuba.core.xml import load_string
-    from mitsuba.python.util import traverse
+    from mitsuba.core import Float, Transform4f, ScalarVector3f, ScalarTransform4f
+    from mitsuba.core.xml import load_dict
 
     @fresolver_append_path
     def make_scene(integrator, spp, param):
-        return load_string("""
-            <scene version="2.0.0">
-                {integrator}
-                <sensor type="perspective">
-                    <string name="fov_axis" value="smaller"/>
-                    <float name="near_clip" value="0.1"/>
-                    <float name="far_clip" value="2800"/>
-                    <float name="focus_distance" value="1000"/>
-                    <transform name="to_world">
-                        <lookat origin="0, 0, 10" target="0, 0, 0" up="0, 1, 0"/>
-                    </transform>
-                    <float name="fov" value="10"/>
-                    <sampler type="independent">
-                        <integer name="sample_count" value="{spp}"/>
-                    </sampler>
-                    <film type="hdrfilm">
-                        <integer name="width" value="64"/>
-                        <integer name="height" value="64"/>
-                        <rfilter type="box"/>
-                    </film>
-                </sensor>
-
-                <shape type="obj" id="light_shape">
-                    <transform name="to_world">
-                        <rotate x="1" angle="180"/>
-                        <translate x="10.0" y="0.0" z="15.0"/>
-                        <translate x="{param}" y="{param}" z="{param}"/>
-                    </transform>
-                    <string name="filename" value="resources/data/obj/xy_plane.obj"/>
-                    <emitter type="smootharea" id="smooth_area_light">
-                        <spectrum name="radiance" value="100"/>
-                    </emitter>
-                </shape>
-
-                <shape type="obj" id="object">
-                    <string name="filename" value="resources/data/obj/smooth_empty_cube.obj"/>
-                    <transform name="to_world">
-                        <translate z="1.0"/>
-                    </transform>
-                </shape>
-
-                <shape type="obj" id="planemesh">
-                    <string name="filename" value="resources/data/obj/xy_plane.obj"/>
-                    <transform name="to_world">
-                        <scale value="2.0"/>
-                    </transform>
-                </shape>
-            </scene>
-        """.format(integrator=integrator, spp=spp, param=param))
+        return load_dict({
+            "type" : "scene",
+            "integrator" : integrator,
+            "sensor" : sensor(spp),
+            "planemesh" : plane_mesh,
+            "light_shape" : {
+                "type" : "obj",
+                "id" : "light_shape",
+                "to_world" : ScalarTransform4f.translate(ScalarVector3f(10, 0, 15) + param) * ScalarTransform4f.rotate([1, 0, 0], 180),
+                "filename" : "resources/data/obj/xy_plane.obj",
+                "smooth_area_light" : {
+                    "type" : "smootharea",
+                    "radiance" : { "type": "spectrum", "value": 100 }
+                }
+            },
+            "object" : {
+                "type" : "obj",
+                "id" : "object",
+                "to_world" : ScalarTransform4f.translate([0, 0, 1]),
+                "filename" : "resources/data/obj/smooth_empty_cube.obj"
+            }
+        })
 
     def get_diff_param(scene):
         # Create a differentiable hyperparameter
         diff_param = Float(0.0)
         ek.set_requires_gradient(diff_param)
 
-        # Update vertices so that they depend on diff_param
-        params = traverse(scene)
-        t = Transform4f.translate(Vector3f(1.0) * diff_param)
-        vertex_positions = params['light_shape.vertex_positions']
-        vertex_positions_t = t.transform_point(vertex_positions)
-        params['light_shape.vertex_positions'] = vertex_positions_t
-
-        # Update the scene
-        params.update()
+        # Create differentiable transform
+        diff_trafo = Transform4f.translate(diff_param)
+        update_vertex_buffer(scene, 'light_shape', diff_trafo)
 
         return diff_param
 
@@ -205,167 +231,97 @@ def test01_light_position(variant_gpu_autodiff_rgb):
 
 @pytest.mark.slow
 def test02_object_position(variant_gpu_autodiff_rgb):
-    from mitsuba.core import Float, Vector3f, Transform4f
-    from mitsuba.core.xml import load_string
+    from mitsuba.core import Float, Transform4f, ScalarTransform4f, ScalarVector3f
+    from mitsuba.core.xml import load_dict
     from mitsuba.python.util import traverse
 
     @fresolver_append_path
     def make_scene(integrator, spp, param):
-        return load_string("""
-            <scene version="2.0.0">
-                {integrator}
-                <sensor type="perspective">
-                    <string name="fov_axis" value="smaller"/>
-                    <float name="near_clip" value="0.1"/>
-                    <float name="far_clip" value="2800"/>
-                    <float name="focus_distance" value="1000"/>
-                    <transform name="to_world">
-                        <lookat origin="0, 0, 10" target="0, 0, 0" up="0, 1, 0"/>
-                    </transform>
-                    <float name="fov" value="10"/>
-                    <sampler type="independent">
-                        <integer name="sample_count" value="{spp}"/>
-                    </sampler>
-                    <film type="hdrfilm">
-                        <integer name="width" value="64"/>
-                        <integer name="height" value="64"/>
-                        <rfilter type="box"/>
-                    </film>
-                </sensor>
-
-                <shape type="obj" id="light_shape">
-                    <transform name="to_world">
-                        <rotate x="1" angle="180"/>
-                        <translate x="10.0" y="0.0" z="15.0"/>
-                    </transform>
-                    <string name="filename" value="resources/data/obj/xy_plane.obj"/>
-                    <emitter type="smootharea" id="smooth_area_light">
-                        <spectrum name="radiance" value="100"/>
-                    </emitter>
-                </shape>
-
-                <shape type="obj" id="object">
-                    <string name="filename" value="resources/data/obj/smooth_empty_cube.obj"/>
-                    <transform name="to_world">
-                        <translate z="1.0"/>
-                        <translate x="{param}"/>
-                    </transform>
-                </shape>
-
-                <shape type="obj" id="planemesh">
-                    <string name="filename" value="resources/data/obj/xy_plane.obj"/>
-                    <transform name="to_world">
-                        <scale value="2.0"/>
-                    </transform>
-                </shape>
-            </scene>
-        """.format(integrator=integrator, spp=spp, param=param))
+        return load_dict({
+            "type" : "scene",
+            "integrator" : integrator,
+            "sensor" : sensor(spp),
+            "planemesh" : plane_mesh,
+            "light_shape" : {
+                "type" : "obj",
+                "id" : "light_shape",
+                "to_world" : ScalarTransform4f.translate([10, 0, 15]) * ScalarTransform4f.rotate([1, 0, 0], 180),
+                "filename" : "resources/data/obj/xy_plane.obj",
+                "smooth_area_light" : {
+                    "type" : "smootharea",
+                    "radiance" : { "type": "spectrum", "value": 100 }
+                }
+            },
+            "object" : {
+                "type" : "obj",
+                "id" : "object",
+                "to_world" : ScalarTransform4f.translate(ScalarVector3f([0, 0, 1]) + param),
+                "filename" : "resources/data/obj/smooth_empty_cube.obj"
+            }
+        })
 
     def get_diff_param(scene):
-
         # Create a differentiable hyperparameter
-        diff_param = mitsuba.core.Float(0.0)
+        diff_param = Float(0.0)
         ek.set_requires_gradient(diff_param)
 
-        # Update vertices so that they depend on diff_param
-        properties = traverse(scene)
-        t = mitsuba.core.Transform4f.translate(mitsuba.core.Vector3f(1.0,0.0,0.0) * diff_param)
-        vertex_positions = properties['object.vertex_positions']
-        vertex_positions_t = t.transform_point(vertex_positions)
-        properties['object.vertex_positions'] = vertex_positions_t
-
-        # Update the scene
-        properties.update()
+        # Create differentiable transform
+        diff_trafo = Transform4f.translate(diff_param)
+        update_vertex_buffer(scene, 'object', diff_trafo)
 
         return diff_param
 
     # Run the test
     check_finite_difference("object_position", make_scene, get_diff_param)
 
+
 # TODO fix this test
 # @pytest.mark.skip
 @pytest.mark.slow
 def test03_envmap(variant_gpu_autodiff_rgb):
-    from mitsuba.core import Float, Vector3f, Transform4f
-    from mitsuba.core.xml import load_string
+    from mitsuba.core import Float, Transform4f, ScalarVector3f, ScalarTransform4f
+    from mitsuba.core.xml import load_dict
     from mitsuba.python.util import traverse
 
     @fresolver_append_path
     def make_scene(integrator, spp, param):
-        return load_string("""
-            <scene version="2.0.0">
-                {integrator}
-                <sensor type="perspective">
-                    <string name="fov_axis" value="smaller"/>
-                    <float name="near_clip" value="0.1"/>
-                    <float name="far_clip" value="2800"/>
-                    <float name="focus_distance" value="1000"/>
-                    <transform name="to_world">
-                        <lookat origin="0, 0, 10" target="0, 0, 0" up="0, 1, 0"/>
-                    </transform>
-                    <float name="fov" value="10"/>
-                    <sampler type="independent">
-                        <integer name="sample_count" value="{spp}"/>
-                    </sampler>
-                    <film type="hdrfilm">
-                        <integer name="width" value="64"/>
-                        <integer name="height" value="64"/>
-                        <rfilter type="gaussian"/>
-                    </film>
-                </sensor>
-
-                <emitter type="envmap">
-                    <float name="scale" value="1"/>
-                    <string name="filename" value="resources/data/envmap/park.hdr"/>
-                    <transform name="to_world">
-                        <rotate x="1.0" angle="90"/>
-                    </transform>
-                </emitter>
-
-                <shape type="obj" id="object">
-                    <string name="filename" value="resources/data/obj/smooth_empty_cube.obj"/>
-                    <transform name="to_world">
-                        <translate z="0.6"/>
-                        <translate x="{param}"/>
-                    </transform>
-                </shape>
-
-                <shape type="obj" id="planemesh">
-                    <string name="filename" value="resources/data/obj/xy_plane.obj"/>
-                    <bsdf type="diffuse">
-                        <rgb name="reflectance" value="0.8 0.8 0.8"/>
-                    </bsdf>
-                    <transform name="to_world">
-                        <scale value="2.0"/>
-                    </transform>
-                </shape>
-            </scene>
-        """.format(integrator=integrator, spp=spp, param=param))
+        return load_dict({
+            "type" : "scene",
+            "integrator" : integrator,
+            "sensor" : sensor(spp),
+            "planemesh" : plane_mesh,
+            "envmap" : {
+                "type" : "envmap",
+                "scale" : 1.0,
+                "filename" : "resources/data/envmap/park.hdr",
+                "to_world" : ScalarTransform4f.rotate([1, 0, 0], 90)
+            },
+            "object" : {
+                "type" : "obj",
+                "id" : "object",
+                "to_world" : ScalarTransform4f.translate(ScalarVector3f([0, 0, 0.6]) + param),
+                "filename" : "resources/data/obj/smooth_empty_cube.obj"
+            }
+        })
 
     def get_diff_param(scene):
-
         # Create a differentiable hyperparameter
-        diff_param = mitsuba.core.Float(0.0)
+        diff_param = Float(0.0)
         ek.set_requires_gradient(diff_param)
 
-        # Update vertices so that they depend on diff_param
-        properties = traverse(scene)
-        t = mitsuba.core.Transform4f.translate(mitsuba.core.Vector3f(1.0, 0.0, 0.0) * diff_param)
-        vertex_positions = properties['object.vertex_positions']
-        vertex_positions_t = t.transform_point(vertex_positions)
-        properties['object.vertex_positions'] = vertex_positions_t
-
-        # Update the scene
-        properties.update()
+        # Create differentiable transform
+        diff_trafo = Transform4f.translate(diff_param)
+        update_vertex_buffer(scene, 'object', diff_trafo)
 
         return diff_param
 
-    diff_integrator = """<integrator type="pathreparam">
-                            <integer name="max_depth" value="2"/>
-                            <float name="kappa_conv_envmap" value="10000000"/>
-                        </integrator>"""
+    diff_integrator = {
+        "type" : "pathreparam",
+        "max_depth" : 2,
+        "kappa_conv_envmap" : 10000000
+    }
 
     # Run the test
-    check_finite_difference("envmap", make_scene, get_diff_param,diff_integrator=diff_integrator, error_threshold=0.1)
+    check_finite_difference("envmap", make_scene, get_diff_param, diff_integrator=diff_integrator, error_threshold=0.1)
 
 # TODO add tests for area+envmap, rotation, scaling, glossy reflection
